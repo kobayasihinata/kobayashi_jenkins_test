f3afe04624128ae7f05354f10593e80d71b2bfca
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2026-01-13 14:24:06 +0900

    mul_div作成

--- jenkins_test/mul_div.cpp
+++ jenkins_test/mul_div.cpp
@@ -1,1 +1,11 @@
 #include "mul_div.h"
+
+int mul_div::mul(int x, int y)
+{
+
+}
+
+int mul_div::div(int x, int y)
+{
+
+}
--- jenkins_test/mul_div.h
+++ jenkins_test/mul_div.h
@@ -1,5 +1,9 @@
 #pragma once
 class mul_div
 {
+public:
+	int mul(int x, int y);
+
+	int div(int x, int y);
 };
 
